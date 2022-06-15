16c82d6863fae87863d8a554aae3ddb6bee88471
Sergey Novitsky <serzh.novicccki7@mail.ru>
2022-06-15 11:24:15 +0300

    third commit

--- src/main/java/com/example/helloworld/HelloApplication.java
+++ src/main/java/com/example/helloworld/HelloApplication.java
@@ -25,6 +25,9 @@ public class HelloApplication extends Application {
         Label label = new Label("Hello World");
         pane.getChildren().add(label);
 
+
+        System.out.println("third master commit");
+
         stage.show();
     }
 
