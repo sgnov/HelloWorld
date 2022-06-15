4fcf68cd9b7dbe6135d235d2e79a260e2ca8bed5
Sergey Novitsky <serzh.novicccki7@mail.ru>
2022-06-15 10:33:31 +0300

    remove fxml

--- src/main/java/com/example/helloworld/HelloApplication.java
+++ src/main/java/com/example/helloworld/HelloApplication.java
@@ -2,18 +2,29 @@ package com.example.helloworld;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage stage) {
+        FlowPane pane = new FlowPane(Orientation.VERTICAL);
+        pane.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(pane, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+
+        Label label = new Label("Hello World");
+        pane.getChildren().add(label);
+
         stage.show();
     }
 
